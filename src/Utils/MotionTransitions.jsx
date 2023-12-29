ac9e20e218ec8356b10a94110604e4cf4ba5795c
danielfranchi555 <danielfranchi3@gmail.com>
2023-12-29 15:31:07 -0300

    commit

--- src/Utils/MotionTransitions.jsx
+++ src/Utils/MotionTransitions.jsx
@@ -33,9 +33,9 @@ export const transitionVariantsPage = {
   export const fadeIn = (direction, delay) => {
     return {
       hidden: {
-        y: direction === "up" ? 80 : direction === "down" ? -80 : 0,
+        y: direction === "up" ? 80 : direction === "down" ? -50 : 0,
         opacity: 0,
-        x: direction === "left" ? 80 : direction === "right" ? -80 : 0,
+        x: direction === "left" ? 80 : direction === "right" ? -50 : 0,
         transition: {
           type: "tween",
           duration: 1.5,
