a49e906d4518098181bb32aa8f32d6b5e7ee2beb
danielfranchi555 <danielfranchi3@gmail.com>
2024-01-16 20:47:52 -0300

    arrow

--- src/components/arrow/Arrow.jsx
+++ src/components/arrow/Arrow.jsx
@@ -1,20 +1,20 @@
-import Image from 'next/image'
-import React from 'react'
-import flecha from '../../../public/flecha.svg'
+// import Image from 'next/image'
+// import React from 'react'
+// import flecha from '../../../public/flecha.svg'
 
-const Arrow = () => {
-  return (
-    <div className='md:flex md:items-center md:justify-center  md:w-[100%] animate-bounce '>
-    <Image
-      src={flecha}
-      width='auto'
-      height='auto'
-      alt='icon'
-      className=''
-    />
+// const Arrow = () => {
+//   return (
+//     <div className='md:flex md:items-center md:justify-center  md:w-[100%] animate-bounce '>
+//     <Image
+//       src={flecha}
+//       width='auto'
+//       height='auto'
+//       alt='icon'
+//       className=''
+//     />
 
-</div>
-  )
-}
+// </div>
+//   )
+// }
 
-export default Arrow
+// export default Arrow
