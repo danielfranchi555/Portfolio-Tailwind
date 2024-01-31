bb79e1eb0e3e4d7632d81db292c96b84681e0728
danielfranchi555 <danielfranchi3@gmail.com>
2024-01-31 10:36:32 -0300

    image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image'
 import React, { useEffect, useRef } from 'react'
-import foto from '../../public/foto-cv-final.png'
+import foto from '../../public/foto-cv-7-min.jpg'
 import FadeLeft from './fade/FadeLeft'
 import FadeRight from './fade/FadeRight'
 import Fade from './fade/Fade'
@@ -20,7 +20,7 @@ const Hero = () => {
       <div className='flex flex-col gap-5 md:flex md:flex-row  py-[64px] md:py-0 md:mb-20  lg:h-[600px]  md:justify-between   md:gap-0  '>
         <article className='flex flex-col gap-4  md:flex md:justify-center'>
           <FadeRight>
-            <h1 className='text-[#ffff] text-4xl md:text-5xl lg:text-7xl   '>hola, mi nombre es Daniel Franchi.</h1>
+            <h1 className='text-[#ffff] text-4xl md:text-5xl lg:text-7xl'>hola, mi nombre es Daniel Franchi.</h1>
           </FadeRight>
           <FadeRight>
             <p className='text-[#C7C7C7] md:text-1xl  lg:text-[18px] max-w-[500px] '>A Sydney based front-end developer passionate about building accessible and user friendly websites.</p>
