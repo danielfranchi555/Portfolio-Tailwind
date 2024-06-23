63cfa0fd92d889b922a1900c16b5958b8ffc2a55
danielfranchi555 <danielfranchi3@gmail.com>
2024-06-23 00:19:18 -0300

    commit

--- src/components/proyects/Proyects.jsx
+++ src/components/proyects/Proyects.jsx
@@ -2,20 +2,25 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { data } from "./data";
-import { Federant } from "next/font/google";
 import Fade from "../fade/Fade";
 import Link from "next/link";
 import { MdArrowOutward } from "react-icons/md";
 import FadeRight from "../fade/FadeRight";
 import FadeLeft from "../fade/FadeLeft";
+import { Badge } from "@/components/ui/badge";
 
 const Proyects = () => {
   const [more, setMore] = useState(false);
   const [mostrarTres, setMostrarTres] = useState(true);
+  const [filter, setFilter] = useState(data);
 
   const toogle = () => {
     console.log("more changed");
     setMostrarTres(!mostrarTres);
+  };
+
+  const filterProyects = (category) => {
+    setFilter(data.filter((prod) => prod.type === category));
   };
 
   return (
@@ -25,10 +30,25 @@ const Proyects = () => {
       </Fade>
       <p className="subtitle-proyects text-[#C7C7C7] lg:text-[18px] lg:max-w-[600px]">
         A continuación, te presento algunos de los proyectos seleccionados que
-        reflejan mi dedicación al desarrollo front-end.
+        reflejan mi dedicación al desarrollo front-end y back transition
+        duration-200-end
       </p>
+      <div className="flex items-center gap-4">
+        <button
+          onClick={() => filterProyects("Landing")}
+          className="border-b px-4 py-2 text-start hover:bg-[#D3E97A]  hover:text-black transition duration-200"
+        >
+          Landing Pages
+        </button>
+        <button
+          onClick={() => filterProyects("Aplications web")}
+          className="border-b px-4 py-2 text-start hover:bg-[#D3E97A]  hover:text-black transition duration-200"
+        >
+          Aplications Web
+        </button>
+      </div>
       <div className="flex flex-col gap-[120px] ">
-        {data.map((item) => (
+        {filter?.map((item) => (
           <div
             key={item.id}
             className="flex flex-col  px-0 gap-2 md:grid md:grid-cols-2"
@@ -50,23 +70,35 @@ const Proyects = () => {
                   INFORMACION DEL PROYECTO
                 </span>
                 <div className="py-2 flex justify-between">
-                  <span className="text-[#FFF]">Año</span>
+                  <span className="text-[14px] text-[#FFF]">Año</span>
                   <span className="text-[#C7C7C7]"> {item.year}</span>
                 </div>
                 <div className="border-b py-2 flex justify-between">
-                  <span className="text-[#ffff]">Rol</span>
+                  <span className="text-[14px] text-[#ffff]">Rol</span>
                   <span className="text-[#C7C7C7]"> {item.role}</span>
+                </div>
+                <div className="border-b py-2 flex justify-between  md:justify-between  gap-2">
+                  <span className="text-[14px] text-[#ffff]  max-w-max">
+                    Technologies
+                  </span>
+                  <div className=" flex justify-end md:flex md:gap-4 w-full md:w-auto gap-2">
+                    {item.technologies?.map((item) => (
+                      <span className="text-[#C7C7C7] md:border rounded-xl flex items-center justify-center md:px-[8px]  text-[10px] md:text-[12px] font-semibold">
+                        {item}
+                      </span>
+                    ))}
+                  </div>
                 </div>
               </div>
               <div className="flex text-[#D3E97A] gap-5">
                 <div className="flex items-center hover:scale-105 hover:shadow-md transition duration-200 ease-in-out">
-                  <Link target="_blank" href={item.linkWeb} >
+                  <Link target="_blank" href={`${item.linkWeb}`}>
                     VER SITIO{" "}
                   </Link>
                   <MdArrowOutward />
                 </div>
                 <div className="flex items-center hover:scale-105 hover:shadow-md transition duration-200 ease-in-out">
-                  <Link target="_blank" href={item.linkGithub}>
+                  <Link target="_blank" href={`${item.linkGithub}`}>
                     VER EN GITHUB{" "}
                   </Link>
                   <MdArrowOutward />
--- src/components/proyects/data.jsx
+++ src/components/proyects/data.jsx
@@ -1,50 +1,56 @@
-import React from 'react'
-import proyectOne from '../../../public/proyect-1-hd.jpg'
-import proyectTwo from '../../../public/page-dentist.png'
-import proyectThree from '../../../public/tienda-online.png'
-import proyectFour from '../../../public/agency-marketing.png'
-import arrow from '../../../public/arrow-blue.svg'
-import proyectoFive from '../../../public/flyer-gym.jpg'
-import proyectSix from '../../../public/flyer-agency.jpg'
-
+import React from "react";
+import proyectOne from "../../../public/proyect-1-hd.jpg";
+import proyectTwo from "../../../public/page-dentist.png";
+import proyectThree from "../../../public/tienda-online.png";
+import proyectFour from "../../../public/agency-marketing.png";
+import arrow from "../../../public/arrow-blue.svg";
+import proyectoFive from "../../../public/flyer-gym.jpg";
+import proyectSix from "../../../public/flyer-agency.jpg";
 
 export const data = [
-   {
-      id: 1,
-      image: proyectSix,
-      name: 'Landing Page',
-      title: 'Agency Marketing',
-      description: 'Landing page desarrollada con NextJs, tailwind css para la customizacion y Framer motion para la implemetacion de animaciones',
-      arrow: arrow,
-      year:'2024',
-      role:'Frontend developer',
-      linkGithub:'https://github.com/danielfranchi555/Agenone',
-      linkWeb: "https://agenone-zk4d.vercel.app/"
-   },
-   {
-      id: 2,
-      image: proyectoFive,
-      name: 'Landing Page',
-      title: 'Gym24',
-      description: 'Landing Page utilizando Nextjs, tailwind css para los estilos y Shadcn para la creacion de algunos componentes ',
-      arrow: arrow,
-      year:'2024',
-      role:'Frontend developer',
-      linkWeb: "https://gym-proyect-lilac.vercel.app/",
-      linkGithub:'https://github.com/danielfranchi555/gym-proyect'
-   },
-   {
-      id: 3,
-      image: proyectThree,
-      name: 'App',
-      title: 'Tienda Online',
-      description: 'Tienda online automatizada para que los productos se manden a WhatsApp usando como base de datos Google Sheets ',
-      arrow: arrow,
-      year:'2024',
-      role:'Frontend developer',
-      linkWeb: "https://agency-marketing-f6z7.vercel.app/",
-      linkGithub:'https://github.com/danielfranchi555/Ecommerce-GoogleSheets'
-   },
-
-]
-
+  {
+    id: 1,
+    image: proyectSix,
+    name: "Landing Page",
+    title: "Agency Marketing",
+    description:
+      "Landing page desarrollada con NextJs, tailwind css para la customizacion y Framer motion para la implemetacion de animaciones",
+    arrow: arrow,
+    year: "2024",
+    role: "Frontend developer",
+    linkGithub: "https://github.com/danielfranchi555/Agenone",
+    linkWeb: "https://agenone-zk4d.vercel.app/",
+    technologies: ["Nextjs", "Tailwind", "Framer Motion", "Shadcn"],
+    type: "Landing",
+  },
+  {
+    id: 2,
+    image: proyectoFive,
+    name: "Landing Page",
+    title: "Gym24",
+    description:
+      "Landing Page utilizando Nextjs, tailwind css para los estilos y Shadcn para la creacion de algunos componentes ",
+    arrow: arrow,
+    year: "2024",
+    role: "Frontend developer",
+    linkWeb: "https://gym-proyect-lilac.vercel.app/",
+    linkGithub: "https://github.com/danielfranchi555/gym-proyect",
+    technologies: ["Nextjs", "Tailwind", "Framer Motion", "Shadcn"],
+    type: "Landing",
+  },
+  {
+    id: 3,
+    image: proyectThree,
+    name: "App",
+    title: "Tienda Online",
+    description:
+      "Tienda online automatizada para que los productos se manden a WhatsApp usando como base de datos Google Sheets ",
+    arrow: arrow,
+    year: "2024",
+    role: "Frontend developer",
+    linkWeb: "https://agency-marketing-f6z7.vercel.app/",
+    linkGithub: "https://github.com/danielfranchi555/Ecommerce-GoogleSheets",
+    technologies: ["GoogleSheets", "React", "Chakra UI"],
+    type: "Aplications web",
+  },
+];
